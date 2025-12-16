e98c9d44b9c9593da0c2ad8e02dee1f42e2f0b73
Ahmad Raza <razarajpoot99598@gmail.com>
2025-12-17 00:06:30 +0500

    fabonacci series - sovled

--- Recursion.js
+++ Recursion.js
@@ -15,7 +15,7 @@ Parts of Recursion
 
 function print(n){
     if(n<1) return;
-    console.log(n);
+  //  console.log(n);
     n = n-1;
     print(n)
 }
@@ -27,14 +27,13 @@ function sumOfn(n){
     return n + sumOfn(n-1)
 }
 
-console.log(sumOfn(15))
+//console.log(sumOfn(15))
 
 // find the n is power of 2
 
 function powerOfTwo(n){
-    console.log(n)
+   // console.log(n)
     if(n==1) {
-        console.log("work?")
         return true;
     }
 
@@ -44,6 +43,70 @@ function powerOfTwo(n){
    return powerOfTwo(n/2)
 }
 let n = 4
-console.log(powerOfTwo(n))
+//console.log(powerOfTwo(n))
 
 
+// sum of all element in array
+
+let arr = [2,3,4,7]
+
+function sum(n){
+    if(n===0) return arr[n];
+
+    return arr[n] + sum(n-1);
+}
+
+//console.log(sum(arr.length-1))
+
+
+
+// sum of all odd numbers in array
+
+function sumOdd(n){
+    if(arr[n]%2!=0){
+        if(n===0) return arr[n];
+
+        return arr[n] + sumOdd(n-1)
+    }else{
+        if(n===0) return arr[n];
+        return sumOdd(n-1)
+    }
+}
+
+//console.log(sumOdd(arr.length-1))
+
+
+//let f = 3
+
+function factorial(f){
+    if(f==0)return 1;
+    
+    return f * factorial(f-1)
+}
+
+//console.log(factorial(f))
+
+// Fibonacchi Series
+
+let p = 0;
+let s = 1;
+let f = p + s;
+
+for(let i = 1; i<=5; i++){
+    f = p + s
+    
+    p = s;
+    s = f;
+    //console.log(f)
+}
+
+//console.log(f)
+
+
+function fib(n){
+    if(n<=1) return n;
+
+    return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(5))
