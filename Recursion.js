52e560f845f383309ca6374f5fd15e589c06ed86
Ahmad Raza <razarajpoot99598@gmail.com>
2025-12-17 21:13:31 +0500

    binary search -solved

--- Recursion.js
+++ Recursion.js
@@ -110,3 +110,23 @@ function fib(n){
 }
 
 console.log(fib(5))
+
+
+var search = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(right>=left){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid]===target){
+            return mid
+        }else if(nums[mid]<target){
+            left = mid + 1;
+        }else{
+            right = mid - 1
+        }
+    }
+    
+    return -1;
+};
