5cc59f733f0a0aaec54452d74a60a6b753d3da07
Ahmad Raza <razarajpoot99598@gmail.com>
2025-12-23 00:46:16 +0500

    updated solution

--- LinkedList.js
+++ LinkedList.js
@@ -201,7 +201,6 @@ testSet.add(4)
 
 console.log(testSet)
 
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -215,32 +214,22 @@ console.log(testSet)
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let zeroNode = new ListNode()
-    let result = new ListNode()
-    let sum = 0;
+    let result = new ListNode();
+    let res = result;
     let carry = 0;
-    digit = 0;
 
-    while(carry>0 || l1.next || l2.next){
-        if(l1===null){
-            l1 = zeroNode
-        }
+    while(carry || l1 || l2){
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val:0) + carry;
+        carry = Math.floor(sum/10)
+        let digit = sum%10;
 
-        if(l2===null){
-            l2 = zeroNode
-        }
-        
-        sum = l1.val + l2.val + carry;
+        let newNode = new ListNode(digit);
+        result.next = newNode;
+        result = newNode;
 
-        if(sum>9){
-            carry = 1
-        }
-
-        digit = sum%10
-        let newNode = new ListNode()
-        newNode.val = digit;
-        
-        result.val = digit
-
+        l1 = l1 && l1.next
+        l2 = l2 && l2.next
     }
+
+    return res.next;
 };
