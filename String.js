fa68e4531dea2538d03eb8590a70d94a72501eb0
Ahmad Raza <razarajpoot99598@gmail.com>
2025-12-25 20:07:55 +0500

    another string problem - solved

--- String.js
+++ String.js
@@ -22,3 +22,23 @@ var balancedStringSplit = function(s) {
     return resCount;
 };
 
+// Valid Palidrom
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase()
+    let rev= "";
+    let filteredString = ""
+
+    for(let i = 0; i<s.length; i++){
+        if(s[i].match(/[a-z0-9]/i)){
+            filteredString += s[i];
+            rev = s[i] + rev
+        }
+    }
+
+    return rev === filteredString
+};
