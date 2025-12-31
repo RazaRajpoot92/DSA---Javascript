1bc25ec4284ff0e096a4a4c72aa6f4654a704cf1
Ahmad Raza <razarajpoot99598@gmail.com>
2025-12-31 22:15:00 +0500

    implemented queue with stacks

--- stack.js
+++ stack.js
@@ -113,3 +113,61 @@ console.log(mystack.empty())
     * var param_4 = obj.empty()
     */
 
+
+
+var MyQueue = function() {
+    this.s = []
+    this.s2 = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.s.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.s2.length === 0){
+        while(this.s.length){
+            this.s2.push(this.s.pop())
+        }
+    }
+
+    return this.s2.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if(this.s2.length===0){
+        while(this.s.length){
+            this.s2.push(this.s.pop())
+        }
+    }
+
+    return this.s2[this.s2.length - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+
+    return this.s.length==0 && this.s2.length==0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+
