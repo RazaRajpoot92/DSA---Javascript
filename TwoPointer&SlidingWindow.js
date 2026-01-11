f53629a7b0acfed5ad1ca88f5f37d72b9a033c37
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-11 21:01:55 +0500

    another section added - two pointer and sliding window

---
+++ TwoPointer&SlidingWindow.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  let map = {}
+
+  for(let i = 0; i < nums.length; i++){
+        map[nums[i]] = i
+  }  
+  
+  for (let i = 0; i<nums.length; i++){
+      let ans = target - nums[i]
+      if(map[ans] && map[ans]!==i){
+          return [i, map[ans]]
+      }
+  }
+  
+};
