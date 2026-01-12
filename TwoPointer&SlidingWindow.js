532b6ad77a3142ee67597bfbdf47c06d903fe3b1
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-12 19:48:21 +0500

    two pointer another problem - solved

--- TwoPointer&SlidingWindow.js
+++ TwoPointer&SlidingWindow.js
@@ -19,3 +19,27 @@ var twoSum = function(nums, target) {
   }
   
 };
+
+//
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let p1 = 0
+    let p2 = numbers.length - 1;
+
+    for(let i = 0; i<numbers.length; i++){
+        if(numbers[p1]+numbers[p2]===target){
+            return [++p1, ++p2]
+        }else if(numbers[p1]+numbers[p2]>target){
+            p2--
+        }else{
+            p1++
+        }
+    }
+};
