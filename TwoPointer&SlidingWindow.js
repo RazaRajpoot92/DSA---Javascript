9d8b005df4008d29d51679cba38c41c9ad81cb76
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-13 21:24:06 +0500

    another two pointer problem - solve

--- TwoPointer&SlidingWindow.js
+++ TwoPointer&SlidingWindow.js
@@ -43,3 +43,28 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if(s.length==0 && t.length==0) return true
+    let i = 0;
+    let j = 0;
+
+    while(j<t.length){
+        if(s[i]===t[j]){
+            i++
+            j++
+        }else{
+            j++
+        }
+
+        if(i==s.length) return true
+    }
+
+    return false
+};
