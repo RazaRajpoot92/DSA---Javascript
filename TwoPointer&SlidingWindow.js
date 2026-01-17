13415c39ce1c8841d26cdda55af1236f51e10493
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-17 17:01:03 +0500

    another problem solved

--- TwoPointer&SlidingWindow.js
+++ TwoPointer&SlidingWindow.js
@@ -227,3 +227,26 @@ var isValid = function(map,k){
 
     return totalCount - max <= k
 }
+
+///
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+    if(n===1||n===2||n===3) return n
+
+    let a = 1;
+    let b = 2;
+    let c = 0;
+    for(let i = 3; i<=n; i++){
+        c = a + b
+        a = b;
+        b = c
+    }
+
+    return c
+};
