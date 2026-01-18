fb2d8b059f98ada4f844bd9115ec89c50f3a4b14
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-18 18:08:30 +0500

    new topic start - Binary Tree

---
+++ BinaryTree.js
@@ -0,0 +1,28 @@
+// PreOrderTraversal
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+
+    let result = []
+
+    function preTraversal(curr){
+        if(!curr) return;
+        result.push(curr.val)
+        preTraversal(curr.left)
+        preTraversal(curr.right)
+    }    
+    preTraversal(root)
+
+    return result
+};
