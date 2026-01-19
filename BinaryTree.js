186f03f6ced8ee3f0a4a52e00d56f960f7d0e048
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-19 21:33:41 +0500

    level order traversal

--- BinaryTree.js
+++ BinaryTree.js
@@ -129,3 +129,41 @@ var postorderTraversal = function(root) {
     return result;
     
 };
+
+// Level order traversal
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [];
+
+    let q = [root]
+    let result = []
+
+    while(q.length){
+        let levelRes = []
+        let level = q.length;
+
+        for(let i = 0; i<level; i++){
+            let curr = q.shift()
+            curr.left && q.push(curr.left)
+            curr.right && q.push(curr.right)
+            levelRes.push(curr.val)
+        }
+
+        result.push(levelRes)
+    }
+
+    return result;
+};
