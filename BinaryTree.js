05b31a0a56fa794d32d23f473cadc443a31105fa
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-22 21:06:23 +0500

    Balanced Binary tree - solved

--- BinaryTree.js
+++ BinaryTree.js
@@ -274,3 +274,39 @@ var isSymmetric = function(root) {
     
     return isMirror(root.left, root.right)
 };
+
+
+///
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    let ans = true;
+    function CalculateHeight(curr){
+        if(!curr) return 0;
+        let leftHeight = CalculateHeight(curr.left);
+        let rightHeight = CalculateHeight(curr.right);
+
+        if(Math.abs(leftHeight - rightHeight) > 1){
+            ans = ans && false
+        }
+
+        return 1 + Math.max(leftHeight, rightHeight)
+    }
+
+    CalculateHeight(root)
+
+    return ans;
+};
+
