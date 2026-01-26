d3f5f75d209d564d2c3484887f84fa5acf6b7849
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-26 23:50:03 +0500

    another problem - solved BST

--- BinaryTree.js
+++ BinaryTree.js
@@ -594,3 +594,42 @@ var searchBST = function(root, val) {
     return ans
     
 };
+
+
+//
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let ans = null
+    let count = k
+
+    function traverse(curr){
+       
+            if(ans != null) return;
+
+           curr.left && traverse(curr.left)
+
+            count--
+            if(count == 0){
+                ans = curr.val
+            }
+            
+           curr.right && traverse(curr.right)
+        
+    }
+    traverse(root)
+    
+    return ans
+};
