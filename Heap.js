f4cf5ad0e46361a031e2e5563d8126b93477fc5d
Ahmad Raza <razarajpoot99598@gmail.com>
2026-01-28 19:17:55 +0500

    maxHeap with prototype-based syntax

--- Heap.js
+++ Heap.js
@@ -2,52 +2,100 @@
 
 
 
-class MinHeap{
+// class MinHeap{
     
-    constructor(){
-        this.heap = []
-    }
+//     constructor(){
+//         this.heap = []
+//     }
     
-    getLeftChildIndex(i){
-        return (2 * i) + 1
-    }
+//     getLeftChildIndex(i){
+//         return (2 * i) + 1
+//     }
     
-    getRightChildIndex(i){
-        return (2 * i) + 2
-    }
+//     getRightChildIndex(i){
+//         return (2 * i) + 2
+//     }
     
-    getParentIndex(i){
-        return Math.floor((i-1)/2)
-    }
+//     getParentIndex(i){
+//         return Math.floor((i-1)/2)
+//     }
     
-    insert(ele){
-        this.heap.push(ele);
-        let currEleIndex = this.heap.length - 1;
-        this.heapifyUp(currEleIndex)
+//     insert(ele){
+//         this.heap.push(ele);
+//         let currEleIndex = this.heap.length - 1;
+//         this.heapifyUp(currEleIndex)
        
-    }
+//     }
     
-    heapifyUp(i){
+//     heapifyUp(i){
         
-        while(i>0){
-             let parentIndex = this.getParentIndex(i)
-            if(this.heap[i] < this.heap[parentIndex]){
-                [this.heap[i], this.heap[parentIndex]] = 
-                [this.heap[parentIndex], this.heap[i]]
-                i = parentIndex;
-            }else{
-                break;
-            }
-        }
+//         while(i>0){
+//              let parentIndex = this.getParentIndex(i)
+//             if(this.heap[i] < this.heap[parentIndex]){
+//                 [this.heap[i], this.heap[parentIndex]] = 
+//                 [this.heap[parentIndex], this.heap[i]]
+//                 i = parentIndex;
+//             }else{
+//                 break;
+//             }
+//         }
         
-    }
+//     }
+// }
+
+
+// const heap = new MinHeap()
+// heap.insert(2)
+// heap.insert(1)
+// heap.insert(3)
+// heap.insert(5)
+// heap.insert(0)
+// console.log(heap.heap)
+
+// MaxHeap
+
+function MaxHeap(){
+    this.heap = []
 }
 
+MaxHeap.prototype.getLeftChildIndex = function(i){
+    return (2 * i) + 1
+}
+MaxHeap.prototype.getRightChildIndex = function(i){
+    return (2 * i) + 2
+}
 
-const heap = new MinHeap()
-heap.insert(2)
+MaxHeap.prototype.getParentIndex = function(i){
+    return Math.floor((i-1)/2)
+}
+
+MaxHeap.prototype.insert = function(ele){
+    this.heap.push(ele)
+    let currEleIndex = this.heap.length - 1
+    this.heapifyUp(currEleIndex)
+}
+
+MaxHeap.prototype.heapifyUp = function(i){
+    while(i>0){
+        
+        let parentIndex = this.getParentIndex(i)
+        
+        if(this.heap[parentIndex]<this.heap[i]){
+            [this.heap[i], this.heap[parentIndex] ] = 
+            [this.heap[parentIndex], this.heap[i]]
+            
+            i = parentIndex
+        }else{
+            break;
+        }
+    }
+    
+}
+
+const heap = new MaxHeap()
+
 heap.insert(1)
-heap.insert(3)
-heap.insert(5)
-heap.insert(0)
+heap.insert(10)
+heap.insert(11)
+
 console.log(heap.heap)
