b4223386dc9c5f035b7920902a6280ae8cfaa110
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-03 17:11:19 +0500

    another question solved

--- Backtracking.js
+++ Backtracking.js
@@ -168,3 +168,36 @@ var combinationSum3 = function(k, n) {
     return result
     
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(arr) {
+
+    let result = []
+    arr.sort((a,b)=>a-b)
+
+    function backtrack(path, choices){
+
+        if(path.length === arr.length){
+            result.push([...path])
+            return;
+        }
+
+        for(let i = 0; i<choices.length; i++){
+            if(i>0 && choices[i]===choices[i-1]) continue;
+
+            path.push(choices[i])
+            backtrack(path, [...choices.slice(0,i), ...choices.slice(i+1)])
+            path.pop()
+        }
+
+    }
+    
+    backtrack([], arr)
+
+    return result;
+    
+};
