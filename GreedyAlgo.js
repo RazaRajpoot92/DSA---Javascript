7dca7f0aa28643ff37ce4dc0db9f127ee6d8efb7
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-05 21:00:01 +0500

    interval problem - solved

--- GreedyAlgo.js
+++ GreedyAlgo.js
@@ -44,3 +44,37 @@ var findContentChildren = function(g, s) {
 
     return i;
 };
+
+//
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let n = intervals.length
+
+    let result = []
+    let i = 0
+
+    // left
+    while(i<n && intervals[i][1] < newInterval[0]){
+        result.push(intervals[i])
+        i++
+    }
+
+    //overlaping
+    while(i<n && intervals[i][0] <= newInterval[1]){
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+        i++
+    }
+    result.push(newInterval)
+    // end intervals
+    while(i<n){
+        result.push(intervals[i])
+        i++
+    }
+    return result
+};
