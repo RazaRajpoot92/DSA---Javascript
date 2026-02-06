480bcdb837a45a30a9bde1027852812053f97488
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-06 17:39:53 +0500

    another problem - solved

--- GreedyAlgo.js
+++ GreedyAlgo.js
@@ -78,3 +78,87 @@ var insert = function(intervals, newInterval) {
     }
     return result
 };
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function (s) {
+
+    let result = []
+    let start = Array(26).fill(-1)
+    let end = Array(26).fill(-1)
+
+    for(let i = 0; i< s.length; i++){
+        let curr = s.charCodeAt(i) - 97
+        
+        if(start[curr] == -1){
+            start[curr] = i
+        }
+        
+        end[curr] = i
+        
+    }
+    
+    let partitionStart = partitionEnd = 0;
+    
+    for(let i = 0; i<s.length; i++){
+        let curr = s.charCodeAt(i) - 97
+        
+        if(partitionEnd < start[curr]){
+            result.push(partitionEnd - partitionStart + 1)
+            partitionStart = partitionEnd = i
+        }
+        
+        partitionEnd = Math.max(partitionEnd, end[curr])
+    }
+    
+    if(partitionEnd - partitionStart + 1 > 0){
+        result.push(partitionEnd - partitionStart + 1)
+    }
+
+    return result
+};
+
+
+
+// approach 1
+// var partitionLabels = function (s) {
+
+//     let i = 0;
+//     let j = 1;
+//     let result = []
+//     let end = 0
+//     while (i < s.length) {
+
+//         if (s[i] == s[j]) {
+//             end = j
+//         }
+
+//         j++
+//         if (j > s.length - 1) {
+//             result.push([i, end])
+//             i++
+//             j = 0
+//         }
+//     }
+
+//     let ans = [result[0]]
+
+//     for (let i = 1; i < result.length; i++) {
+//         if (result[i][0] <= ans[ans.length - 1][1]) {
+//             ans[ans.length - 1][1] = Math.max(ans[ans.length - 1][1], result[i][1])
+//         } else {
+//             ans.push(result[i])
+//         }
+//     }
+
+
+//     let final = []
+
+//     for (let i = 0; i < ans.length; i++) {
+//         final.push((ans[i][1] - ans[i][0]) + 1)
+//     }
+
+//     return final
+// };
--- Test.js
+++ Test.js
@@ -1,14 +1,20 @@
-function write(s){
+// function write(s){
 
-    for(var i = 0; i<s.length; i++){
-        function close(i){
-            setTimeout(() => {
-            process.stdout.write(s[i])
-         }, i * 150);
-        }
-        close(i)
-    }
+//     for(var i = 0; i<s.length; i++){
+//         function close(i){
+//             setTimeout(() => {
+//             process.stdout.write(s[i])
+//          }, i * 150);
+//         }
+//         close(i)
+//     }
 
-}
-let s = "hello world, how I can help you?"
-write(s)
+// }
+// let s = "hello world, how I can help you?"
+// write(s)
+
+let count = 0;
+document.getElementById("clickMe").addEventListener("click",function xy(){
+    console.log("work", ++count)
+    
+})
