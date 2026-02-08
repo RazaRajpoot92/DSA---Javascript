3bb20eaace3a1842d4f86141ed468068c410ce30
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-08 21:57:22 +0500

    completed Greedy algo

--- GreedyAlgo.js
+++ GreedyAlgo.js
@@ -254,3 +254,88 @@ var canCompleteCircuit = function(gas, cost) {
 
     return totalGain < 0? -1: ans
 };
+
+//
+
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function (arr) {
+    let n = arr.length;
+    let ans = n
+    let i = 1;
+
+    while (i < n) {
+
+        if (i < n && arr[i] == arr[i - 1]) {
+            i++
+            continue
+        }
+
+        let up = 0;
+        while (i < n && arr[i] > arr[i - 1]) {
+            up++
+            ans += up
+            i++
+        }
+
+        let down = 0;
+        while (i < n && arr[i] < arr[i - 1]) {
+            down++
+            ans += down
+
+            i++
+        }
+        ans = ans - Math.min(up, down)
+    }
+
+    return ans
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var candy = function(arr) {
+//     let n = arr.length;
+
+//     let ltr = new Array(n).fill(1)
+
+//     for(let i = 1; i< n; i++){
+//         if(arr[i]>arr[i-1]){
+//             ltr[i] = ltr[i-1] + 1
+//         }
+//     }
+
+//     let rtl = new Array(n).fill(1)
+
+//     for(let i = n-2; i>=0; i--){
+//         if(arr[i]>arr[i+1]){
+//             rtl[i] = rtl[i+1] + 1
+//         }
+//     }
+
+//     let ans = 0;
+
+//     for(let i =0; i<n; i++){
+//         ans = ans + Math.max(ltr[i], rtl[i])
+//     }
+
+//     return ans
+// };
