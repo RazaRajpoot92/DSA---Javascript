437b7e1849ceaa28a8ef086940410a8ca69066ee
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-08 17:52:40 +0500

    another greedy problem - solved

--- GreedyAlgo.js
+++ GreedyAlgo.js
@@ -224,3 +224,33 @@ var carPooling = function(trips, capacity) {
 
     return true
 };
+
+
+//
+
+
+
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    let totalGain = 0;
+    let currGain = 0;
+    let ans = 0;
+
+    for(let i = 0; i< gas.length; i++){
+        let gain = gas[i] - cost[i]
+
+        currGain += gain;
+        totalGain += gain;
+
+        if(currGain < 0){
+            ans = i+1;
+            currGain = 0;
+        }
+    }
+
+    return totalGain < 0? -1: ans
+};
