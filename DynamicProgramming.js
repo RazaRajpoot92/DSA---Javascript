06ff1d9c151d8ce776c837705d36716171645f2e
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-10 03:21:47 +0500

    DP problem - solved

--- DynamicProgramming.js
+++ DynamicProgramming.js
@@ -40,3 +40,56 @@ var fib = function(n) {
 
     return dp[n]
 };
+
+
+// Climbing stairs
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var climbStairs = function(n) {
+    
+    let dp = [0,1,2]
+
+    for(let i = 3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n]
+     
+};
+
+
+
+// let dp = {}
+// var climbStairs = function(n) {
+    
+//     if(n<=2) return n
+
+//     if(!dp[n]){
+//         dp[n] = climbStairs(n-1) + climbStairs(n-2)
+//     }
+
+//     return dp[n]  
+// };
+
+
+
+// var climbStairs = function(n) {
+    
+//     if(n===1||n===2||n===3) return n
+
+//     let a = 1;
+//     let b = 2;
+//     let c = 0;
+//     for(let i = 3; i<=n; i++){
+//         c = a + b
+//         a = b;
+//         b = c
+//     }
+
+//     return c
+// };
