97906ce1e8c3e489fb1690f8045d93bd2fbf744b
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-09 17:58:59 +0500

    Tabulation approach added

--- DynamicProgramming.js
+++ DynamicProgramming.js
@@ -26,3 +26,17 @@ var fib = function(n) {
 //     if(n<=1) return n;
 //     return fib(n-1) + fib(n-2);
 // };
+
+
+// Bottom up => Tabulation | Approach
+
+
+var fib = function(n) {
+    let dp = [0,1]
+
+    for(let i = 2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n]
+};
