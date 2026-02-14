9288433a13def39df189c53e0c48fbdfe64c310e
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-15 04:52:07 +0500

    another problem - solved DP

--- DynamicProgramming.js
+++ DynamicProgramming.js
@@ -373,3 +373,39 @@ var lengthOfLIS = function (arr) {
     return lisLength;
 
 };
+
+//
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (arr) {
+
+    let sum = arr.reduce((acc, curr) => acc + curr, 0) 
+
+    if (sum % 2) return false
+
+    sum = sum/2
+    let dp = Array.from({length: sum+1}, ()=> Array(arr.length).fill(undefined))
+    let fn = (remS, start) => {
+
+        if (remS === 0) return true;
+
+        if (remS < 0) return false;
+
+        if(dp[remS][start] != undefined) return dp[remS][start]
+
+        for (let i = start; i < arr.length; i++) {
+
+            if (fn(remS - arr[i], i + 1)) {
+                return dp[remS][start] = true
+            }
+        }
+
+        return dp[remS][start] = false
+    }
+
+
+    return fn(sum, 0)
+};
