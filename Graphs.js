804ea442e855652f00fd0764643b17e90a461ad4
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-16 19:48:30 +0500

    new data structure started - Graphs

---
+++ Graphs.js
@@ -0,0 +1,43 @@
+/*
+Graphs are data structures that represent relationship or connection between objects
+
+*/
+
+
+
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    if(!node) return null;
+
+    let q = [node];
+    let cloneNode = new Node(node.val)
+    let visited = new Map()
+    visited.set(node, cloneNode)
+
+    while(q.length){
+        let curr = q.shift()
+
+        for(n of curr.neighbors){
+            if(!visited.has(n)){
+                q.push(n)
+                visited.set(n, new Node(n.val))
+            }
+
+            let currClone = visited.get(curr)
+            currClone.neighbors.push(visited.get(n))
+        }
+    }
+
+    return cloneNode
+};
