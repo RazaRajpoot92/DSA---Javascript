8ff36e366c04b3d8ee1e6bc83ed11fe8af84a6f4
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-17 17:48:57 +0500

    find ifpath is exists in graph

--- Graphs.js
+++ Graphs.js
@@ -80,3 +80,42 @@ var cloneGraph = function(node) {
 
     return cloneNode;
 };
+//
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    if(n == 1) return true
+    let map = {};
+
+    for(let [x,y] of edges){
+        if(!map[x]) map[x] = [];
+        if(!map[y]) map[y] = [];
+        map[x].push(y);
+        map[y].push(x);
+    }
+
+    let q = [source];
+    let visited = new Set();
+    visited.add(source)
+
+    while(q.length){
+        let curr = q.shift()
+
+        if(curr === destination) return true
+
+        for(let neighbor of map[curr]){
+            if(!visited.has(neighbor)){
+                q.push(neighbor);
+                visited.add(neighbor)
+            }
+        }
+    }
+
+    return false;
+};
