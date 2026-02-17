a17adf9ae201220b815deca1388bac6de34a6aaa
Ahmad Raza <razarajpoot99598@gmail.com>
2026-02-17 19:15:21 +0500

    recursive DFS for graph

--- Graphs.js
+++ Graphs.js
@@ -119,3 +119,38 @@ var validPath = function(n, edges, source, destination) {
 
     return false;
 };
+
+
+ // Recursive DFS
+var validPath = function(n, edges, source, destination) {
+    if(n == 1) return true
+    let map = {};
+
+    for(let [x,y] of edges){
+        if(!map[x]) map[x] = [];
+        if(!map[y]) map[y] = [];
+        map[x].push(y);
+        map[y].push(x);
+    }
+
+    let visited = new Set();
+
+    function DFS(curr){
+        visited.add(curr);
+        if(curr == destination) return true;
+
+        for(let neighbor of map[curr]){
+            if(!visited.has(neighbor)){
+
+               if(DFS(neighbor)) return true;
+                
+            }
+        }
+
+        return false;
+
+    }
+
+    return DFS(source)
+    
+};
