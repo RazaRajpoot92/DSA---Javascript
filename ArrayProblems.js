c8f8cda35fa3aa2b2eb74e1cb4993b9c1a2e7982
Ahmad Raza <razarajpoot99598@gmail.com>
2026-05-16 21:41:56 +0500

    binary search problem solution added

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -311,3 +311,49 @@ var findMin = function(arr) {
 
     return arr[r]
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var findMin = function(nums) {
+//     let l = 0;
+//     let r = nums.length - 1
+
+//     while(l<r){
+//         while(l<r && nums[l] == nums[l+1]) l++;
+//         while(l<r && nums[r] == nums[r-1]) r--;
+
+//         let mid = Math.floor(l + (r - l) /2)
+
+//         if(nums[mid] > nums[r]){
+//             l = mid + 1
+//         }else{
+//             r = mid
+//         }
+//     }
+
+//     return nums[r]
+// };
+
+
+var findMin = function(nums) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while(l < r){
+        let mid = Math.floor(l + (r - l) / 2);
+
+        if(nums[mid] > nums[r]){
+            l = mid + 1;
+        }
+        else if(nums[mid] < nums[r]){
+            r = mid;
+        }
+        else{
+            r--;
+        }
+    }
+
+    return nums[l];
+};
