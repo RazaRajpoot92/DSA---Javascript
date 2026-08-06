e72ac5f1b80d0a33b53d9de2bbd4808893b6d7f6
Ahmad Raza <razarajpoot99598@gmail.com>
2026-08-06 05:20:19 +0500

    solved easy problem

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -1224,3 +1224,21 @@ var maximumProduct = function(nums) {
 
     return ans;
 };
+
+var smallestNumber = function(n, t) {
+    
+  while(true){
+    let num = n
+    let product = 1;
+    while(num>0){
+        let digit = num%10;
+        num = Math.floor(num/10);
+        product *= digit;
+    }
+
+    if(product%t == 0) return n;
+
+    n++
+
+  }  
+};
