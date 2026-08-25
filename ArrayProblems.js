1f2a697715d8de1d4052eeb7e6cb2def4bdb9c39
Ahmad Raza <razarajpoot99598@gmail.com>
2026-08-25 13:44:36 +0500

    solved my own, easy problem

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -1533,3 +1533,20 @@ var sumGame = function(num) {
 
     return true;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingMultiple = function(nums, k) {
+    let set = new Set(nums);
+
+    let multiple = k;
+
+    while(set.has(multiple)){
+        multiple += k
+    }
+
+    return multiple
+};
